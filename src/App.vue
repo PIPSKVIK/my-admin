f6ae1d5aca34ecbe08349dca441499449afd90c6
Alexander Milovanov <milovanska@gmail.com>
2022-03-10 18:58:56 +0300

    feat: add scss var/mixn-etc - add primevue toast

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,70 @@
-<script setup></script>
+<script setup>
+import { useToast } from "primevue/usetoast";
+const $toast = useToast();
+
+
+const items = [
+  {
+    label: "Update",
+    icon: "pi pi-refresh",
+    command: () => {
+      $toast.add({
+        severity: "success",
+        summary: "Updated",
+        detail: "Data Updated",
+        life: 3000,
+      });
+    },
+  },
+  {
+    label: "Delete",
+    icon: "pi pi-times",
+    command: () => {
+      $toast.add({
+        severity: "warn",
+        summary: "Delete",
+        detail: "Data Deleted",
+        life: 3000,
+      });
+    },
+  },
+  {
+    label: "Vue Website",
+    icon: "pi pi-external-link",
+    command: () => {
+      window.location.href = "https://vuejs.org/";
+    },
+  },
+  {
+    label: "Upload",
+    icon: "pi pi-upload",
+    to: "/fileupload",
+  },
+];
+</script>
 
 <template>
   <div id="app">
-    <v_button></v_button>
+    <div class="test-container">
+      <Button
+        label="text"
+        class="p-button p-component p-button-raised p-button-text p-button-plain"
+      />
+    </div>
+    <SplitButton label="Save" icon="pi pi-plus" :model="items" />
+    <Toast />
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.test-container {
+  display: flex;
+  justify-content: center;
+
+  margin: 0 auto;
+  max-width: 500px;
+  padding: 100px;
+  background-color: var(--color-background-soft);
+  border-radius: var(--radius-default);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import PrimeVue from "primevue/config";
+import ToastService from 'primevue/toastservice';
 import App from "./App.vue";
 import router from "./router";
 const app = createApp(App);
@@ -11,18 +12,24 @@ import "./styles/index.scss";
 
 import "primevue/resources/themes/md-dark-indigo/theme.css"; //theme
 import "primevue/resources/primevue.min.css"; //core css
-// import "primeicons/primeicons.css"; //icons
+import "primeicons/primeicons.css"; //icons
+
 
 app.use(createPinia());
 app.use(router);
 app.use(PrimeVue);
+app.use(ToastService);
 
 // __________Prime__________
-import v_button from "primevue/button";
-import PInputText from "primevue/inputtext";
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+import SplitButton from 'primevue/splitbutton';
+import Toast from 'primevue/toast';
 
-app.component("v_button", v_button);
-app.component("PInputText", PInputText);
+app.component("Button", Button);
+app.component("InputText", InputText);
+app.component("SplitButton", SplitButton);
+app.component("Toast", Toast);
 // __________Prime__________
 
 app.mount("#app");
