2abff5cd26f5f291dbf8e55ba80c23b6682e71ab
Alexander Milovanov <milovanska@gmail.com>
2022-03-11 17:45:06 +0300

    feat: add mixin scss

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,12 @@
 <script setup>
-// import { useToast } from "primevue/usetoast";
-import { RouterView } from "vue-router";
-// const $toast = useToast();
-
-// const items = [
-//   {
-//     label: "Update",
-//     icon: "pi pi-refresh",
-//     command: () => {
-//       $toast.add({
-//         severity: "success",
-//         summary: "Updated",
-//         detail: "Data Updated",
-//         life: 3000,
-//       });
-//     },
-//   },
-//   {
-//     label: "Delete",
-//     icon: "pi pi-times",
-//     command: () => {
-//       $toast.add({
-//         severity: "warn",
-//         summary: "Delete",
-//         detail: "Data Deleted",
-//         life: 3000,
-//       });
-//     },
-//   },
-//   {
-//     label: "Vue Website",
-//     icon: "pi pi-external-link",
-//     command: () => {
-//       window.location.href = "https://vuejs.org/";
-//     },
-//   },
-//   {
-//     label: "Upload",
-//     icon: "pi pi-upload",
-//     to: "/fileupload",
-//   },
-// ];
+import CompositionExample from '@/components/CompositionExample.vue'
 </script>
 
 <template>
   <div id="app">
-    <!-- <div class="test-container">
-      <Button
-        label="text"
-        class="p-button p-component p-button-raised p-button-text p-button-plain"
-      />
-    </div>
-    <SplitButton label="Save" icon="pi pi-plus" :model="items" />
-    <Toast /> -->
+    <CompositionExample />
     <RouterView />
   </div>
 </template>
 
-<style lang="scss">
-// .test-container {
-//   display: flex;
-//   justify-content: center;
-
-//   margin: 0 auto;
-//   max-width: 500px;
-//   padding: 100px;
-//   background-color: var(--color-background-soft);
-//   border-radius: var(--radius-default);
-// }
-</style>
+<style lang="scss"></style>
